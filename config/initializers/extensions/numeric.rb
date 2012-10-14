e7f5acbe6e17d9438b03ba13b014cd2908bed442
Pete Michaud <michaudp@gmail.com>
2012-10-14 13:44:35 -0500

    removed old files

--- config/initializers/extensions/numeric.rb
+++
@@ -1,5 +0,0 @@
-class Numeric
-  def time_sentence specificity = 3
-    Time.sentence(self, specificity)
-  end
-end
--- spec/time_sentence/time_sentence_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Numeric do
-
-  it "should accept a positive integer" do
-    100000000.time_sentence.should eql "3 years, 5 months, 1 week"
-  end
-
-end
