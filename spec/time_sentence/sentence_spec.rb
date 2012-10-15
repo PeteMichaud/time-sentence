8074280a4c41b611b1a2c494d260b29233298123
Pete Michaud <michaudp@gmail.com>
2012-10-15 12:20:18 -0500

    updated tests with new method names

--- spec/time_sentence/sentence_spec.rb
+++ spec/time_sentence/sentence_spec.rb
@@ -3,36 +3,36 @@ require 'spec_helper'
 describe 'Time.sentence' do
 
   it "should accept a positive integer" do
-    Time.sentence(100000000).should eql "3 years, 5 months, 1 week"
-    100000000.time_sentence.should eql "3 years, 5 months, 1 week"
+    Time.to_sentence(100000000).should eql "3 years, 5 months, 1 week"
+    100000000.to_time_sentence.should eql "3 years, 5 months, 1 week"
   end
 
   it "should accept a negative integer" do
-    Time.sentence(-3600).should eql "1 hour ago"
-    -3600.time_sentence.should eql "1 hour ago"
+    Time.to_sentence(-3600).should eql "1 hour ago"
+    -3600.to_time_sentence.should eql "1 hour ago"
   end
 
   it "should accept 0" do
-    Time.sentence(0).should eql "now"
-    0.time_sentence.should eql "now"
+    Time.to_sentence(0).should eql "now"
+    0.to_time_sentence.should eql "now"
   end
 
   it "should accept a very large number" do
-    Time.sentence(100000000000000000000000).should be_kind_of String
-    100000000000000000000000.time_sentence.should be_kind_of String
+    Time.to_sentence(100000000000000000000000).should be_kind_of String
+    100000000000000000000000.to_time_sentence.should be_kind_of String
   end
 
   it "should accept specificity" do
     (1..10).each do |specificity|
-      Time.sentence(123456789123456789, specificity).should be_kind_of String
+      Time.to_sentence(123456789123456789, specificity).should be_kind_of String
       123456789123456789.time_sentence(specificity).should be_kind_of String
     end
   end
 
   it "should ignore a specificity that is out of range" do
-      Time.sentence(123456789123456789, -5).should be_kind_of String
+      Time.to_sentence(123456789123456789, -5).should be_kind_of String
       123456789123456789.time_sentence(-5).should be_kind_of String
-      Time.sentence(123456789123456789, 20).should be_kind_of String
+      Time.to_sentence(123456789123456789, 20).should be_kind_of String
       123456789123456789.time_sentence(20).should be_kind_of String
   end
 
